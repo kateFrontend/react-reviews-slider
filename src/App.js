883c55a328648f596fa874fa1b4226e572ad67e5
kateFrontend <ecdanilova@gmail.com>
2022-11-18 15:21:29 -0500

    install icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <h2>slider project setup</h2>;
 }
 
-export default App
+export default App;
